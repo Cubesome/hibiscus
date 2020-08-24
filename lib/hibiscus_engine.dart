c73dbf1cf933ea9f74872bd66d1852cd39ceaa85
Konrad Kubczyk <konrad.kubczyk@gmail.com>
2020-08-24 17:20:06 +0200

    Added a missing space.

--- lib/hibiscus_engine.dart
+++ lib/hibiscus_engine.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-Future<String> hibiscusEngine({@required String textToProcess, @required double enhancementIntensity, @required bool textAlteration, @required bool kaomojiInsertion,@required bool commentsInsertion}) async {
+Future<String> hibiscusEngine({@required String textToProcess, @required double enhancementIntensity, @required bool textAlteration, @required bool kaomojiInsertion, @required bool commentsInsertion}) async {
   await new Future.delayed(
     const Duration(seconds: 2),
   ); // TODO: Remove this simulation of text processing when the engine is completed.
